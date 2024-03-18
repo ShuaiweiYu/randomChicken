369ef08d66be9a3655e75b61d3a6386e468beb30
Shuaiwei Yu <shuaiwei.yu@tum.de>
2024-03-18 15:35:07 +0100

    backend setup

---
+++ randomChicken/src/main/java/tum/shuaiwei/randomchicken/RandomChickenApplication.java
@@ -0,0 +1,13 @@
+package tum.shuaiwei.randomchicken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomChickenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomChickenApplication.class, args);
+    }
+
+}
---
+++ randomChicken/src/test/java/tum/shuaiwei/randomchicken/RandomChickenApplicationTests.java
@@ -0,0 +1,13 @@
+package tum.shuaiwei.randomchicken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomChickenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
